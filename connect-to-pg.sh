ffd0bca84782a4fb56670a3f397adb8d81ba95c8
Arsen Arsenović <arsen@aarsen.me>
2025-04-16 19:23:41 +0200

    move to helm

--- connect-to-pg.sh
+++ connect-to-pg.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 PGPASSWORD="$(kubectl get secret banka-4-pguser-"$(whoami)" -o jsonpath='{.data.password}' | base64 -d)"
 export PGPASSWORD
-kubectl port-forward pod/banka-4-instance1-2dzb-0 15432:5432 &
+kubectl port-forward pod/"$(kubectl get pods --output=json -l postgres-operator.crunchydata.com/role=master | jq -r .items[0].metadata.name)" 15432:5432 &
 sleep 1
-psql -p 15432 -d user-service -h localhost "$@"
+psql -p 15432 -d bank-service -h localhost "$@"
 status="$?"
 trap 'kill %1' EXIT
 exit "${status}"
