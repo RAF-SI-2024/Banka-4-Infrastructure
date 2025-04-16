dddc614b1be4dbda4a7616e48bcb347bdb675c6b
Arsen Arsenović <arsen@aarsen.me>
2025-04-16 20:39:35 +0200

    fix secret path in connect-to-pg.sh

--- connect-to-pg.sh
+++ connect-to-pg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PGPASSWORD="$(kubectl get secret banka-4-pguser-"$(whoami)" -o jsonpath='{.data.password}' | base64 -d)"
+PGPASSWORD="$(kubectl get secret banka-4-backend-pguser-"$(whoami)" -o jsonpath='{.data.password}' | base64 -d)"
 export PGPASSWORD
 kubectl port-forward pod/"$(kubectl get pods --output=json -l postgres-operator.crunchydata.com/role=master | jq -r .items[0].metadata.name)" 15432:5432 &
 sleep 1
